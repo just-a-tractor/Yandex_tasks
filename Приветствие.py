98d1dd6c60f2aaa9815de1c8c089a7041cc8fe57
just-a-tractor <45266305+just-a-tractor@users.noreply.github.com>
2019-04-12 14:18:42 +0300

    Update Приветствие.py

--- Приветствие.py
+++ Приветствие.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/greeting_form', methods=['GET', 'POST'])
 def greeting_form():
     if request.method == 'GET':
-        return render_template('index.html', name='Васян')
+        return render_template('index1.htm', name='Васян')
     elif request.method == 'POST':
         name = request.form['name']
         return render_template('index.html', name=name)
